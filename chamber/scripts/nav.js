4fd738f7e8090d5eb2cb2745c3a808c6881c22e1
Mirabel Nwanneka <memeruwa@gmail.com>
2025-11-18 23:17:12 +0100

    yes fixing

--- chamber/scripts/nav.js
+++ chamber/scripts/nav.js
@@ -28,3 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("timestamp").value = new Date().toISOString();
+});
+
+
